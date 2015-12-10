ac70fc6be6a91899e90645d293383766e7e40c5d
Hub-er <cloudroute@163.com>
2015-12-10 23:41:52 +0800

    Update ck_main.sh

--- backup_rman/ck_main.sh
+++ backup_rman/ck_main.sh
@@ -2,17 +2,17 @@
 #
 #
 #
-mail_list="for_monitor@foxmail.com"
+mail_list="for_monitor@xxx.com"
 set -x
 
 curpath=/home/tendyron/sql_check
-script_for_backup=$curpath/sc_rman_159backup.sh
-report_for_backup=$curpath/rp_rman_159backup.txt
+script_for_backup=$curpath/sc_rman_backup.sh
+report_for_backup=$curpath/rp_rman_backup.txt
 
-ssh oracle@172.16.10.159 'bash -s' < $script_for_backup > $report_for_backup
+ssh oracle@172.16.10.xxx 'bash -s' < $script_for_backup > $report_for_backup
 
 if [ `cat $report_for_backup | wc -l` -gt 0 ]; then
-        cat $report_for_backup | mutt -s "rman backup for 159 auddb"  $mail_list
+        cat $report_for_backup | mutt -s "rman backup for auddb"  $mail_list
 else
-        mutt -s "rman backup for 159 auddb" $mail_list < "no result present.maybe error occurs!"
+        mutt -s "rman backup for auddb" $mail_list < "no result present.maybe error occurs!"
 fi
