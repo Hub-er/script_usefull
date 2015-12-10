059f82bc6dc1506a20bad5de6f29704e5359f595
Hub-er <cloudroute@163.com>
2015-12-10 23:37:32 +0800

    Create ck_main.sh
    远程通过rman备份。

---
+++ backup_rman/ck_main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#
+#
+mail_list="for_monitor@foxmail.com"
+set -x
+
+curpath=/home/tendyron/sql_check
+script_for_backup=$curpath/sc_rman_159backup.sh
+report_for_backup=$curpath/rp_rman_159backup.txt
+
+ssh oracle@172.16.10.159 'bash -s' < $script_for_backup > $report_for_backup
+
+if [ `cat $report_for_backup | wc -l` -gt 0 ]; then
+        cat $report_for_backup | mutt -s "rman backup for 159 auddb"  $mail_list
+else
+        mutt -s "rman backup for 159 auddb" $mail_list < "no result present.maybe error occurs!"
+fi
